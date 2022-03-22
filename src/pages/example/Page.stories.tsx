ab208c9d45744ebe8d34680eef04ff5045a83cd8
sejas <antonio@sejas.es>
2022-03-22 15:47:23 +0000

    feat: remove interactions from page

--- src/pages/example/Page.stories.tsx
+++ src/pages/example/Page.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+// import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
 export default {
@@ -19,8 +19,3 @@ export const LoggedOut = Template.bind({});
 export const LoggedIn = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
-};
