5aead11a9f8b8af39414635420c0c4ba6eb60dfe
sejas <antonio@sejas.es>
2022-03-22 15:39:03 +0000

    feat: add mui provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import {THEME} from '../src/theme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +11,13 @@ export const parameters = {
     },
   },
 }
+
+
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={THEME}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
