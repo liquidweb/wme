b80345ec0e9de2541263150ba570e1e6adb8fa33
sejas <antonio@sejas.es>
2022-03-23 13:12:02 +0000

    feat(theming): change logo and link

---
+++ .storybook/storybook-theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'WME Library',
+  brandUrl: 'https://tri.be/',
+  brandImage: 'https://user-images.githubusercontent.com/779993/159707093-c8e1f4d8-2358-4216-8a15-0a9e8616306d.png',
+});
