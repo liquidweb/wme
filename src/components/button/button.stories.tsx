a0991b475f39d829e2a4c9fe45bb2b1bfa4617a6
coltenvantussenbrook <coltenvantussenbrook@gmail.com>
2022-03-28 16:27:24 -0600

    add buttons with latest merge

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,40 +1,65 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import DownhillSkiingIcon from '@mui/icons-material/DownhillSkiing';
 
-import { Button } from '..';
-
+import { Button } from '../';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Components/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+	title: 'Components/Button',
+	component: Button,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+	argTypes: {
+	  color: {
+	    options: ['primary', 'secondary', 'inherit'],
+	    control: 'radio',
+	  },
+		startIcon: {
+			control: false,
+			mapping: {
+				true: <DownhillSkiingIcon />
+			}
+		},
+		endIcon: {
+			control: 'boolean',
+			mapping: {
+				true: <DownhillSkiingIcon />
+			}
+		},		
+	},
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 const commonArgs = {
-  children: 'Button',
+    children: 'Button',
+	disabled: false,
+}
+
+export const Primary = Template.bind({});
+Primary.args = {
+    ...commonArgs,
+    variant: 'contained',
+    color: 'primary',
 };
 
-export const Text = Template.bind({});
+export const Secondary = Template.bind({});
+Secondary.args = {
+    ...commonArgs,
+    variant: 'contained',
+    color: 'secondary',
+};
+
+export const Stroked = Template.bind({});
+Stroked.args = {
+    ...commonArgs,
+    variant: 'outlined',
+    color: 'primary',
+};
+
+export const NoContainer = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Text.args = {
-  ...commonArgs,
-  variant: 'text',
-};
-
-export const Contained = Template.bind({});
-Contained.args = {
-  ...commonArgs,
-  variant: 'contained',
-};
-
-export const Outlined = Template.bind({});
-Outlined.args = {
-  ...commonArgs,
-  variant: 'outlined',
+NoContainer.args = {
+    ...commonArgs,
+    variant: 'text',
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
 import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 interface ButtonProps extends MuiButtonProps {
-
+    color?: 'primary' | 'secondary'| 'inherit';
+    variant?: 'outlined' | 'contained' | 'text';
 }
 
-const BUTTON_SX = {
+const StyledButton = styled(MuiButton)<ButtonProps>(({ theme }) => ({
+    textTransform: 'none',
+    padding: '6px 12px',
+    boxShadow: 'none',
+    '&:hover': {
+        boxShadow: 'none',
+    },
+	'&.MuiButton-contained&.Mui-disabled': {
+		backgroundColor: theme.palette.text.placeholder,
+		color: theme.palette.text.white,
+	},
+    '&.MuiButton-text': {
+        color: theme.palette.text.primary,
+        '&:focus, &:hover': {
+            backgroundColor: theme.palette.background.hover,
+        },
+    },
+	'&.MuiButton-text&.Mui-disabled': {
+		backgroundColor: 'transparent',
+		color: theme.palette.text.placeholder,
+	},
+	'&.MuiButton-outlined&.Mui-disabled': {
+		backgroundColor: 'transparent',
+		color: theme.palette.text.placeholder,
+	},	
+}));
 
+const Button: React.FC<ButtonProps> = (props) => {
+    return <StyledButton {...props} />;
 };
 
-export default function Button(props: ButtonProps) {
-  const { sx = {}, ...rest } = props;
-  return <MuiButton color="primary" sx={{ ...BUTTON_SX, ...sx }} {...rest} />;
-}
+export default Button;
