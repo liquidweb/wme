0559ef73fab123fb4cb8b8c467225f577933a5ad
Mark Furrow <mark@tri.be>
2022-04-01 12:36:11 -0700

    SetupCard: Adds styled options; Adjusts padding

--- src/components/setup-card/setup-card.tsx
+++ src/components/setup-card/setup-card.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import Card, { CardProps } from '@mui/material/Card';
 
-const StyledSetupCard = styled(Card)<CardProps>(({ theme }) => ({
+const StyledSetupCard = styled(Card, {
+  name: 'WmeSetupCard',
+  slot: 'Root',
+})<CardProps>(({ theme }) => ({
   marginRight: 'auto',
   marginLeft: 'auto',
   maxWidth: theme.spacing(100),
@@ -12,6 +15,7 @@ const StyledSetupCard = styled(Card)<CardProps>(({ theme }) => ({
     marginTop: theme.spacing(4),
     paddingTop: 0,
     paddingRight: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
     paddingLeft: theme.spacing(4),
 
     '& .MuiListItemIcon-root': {
