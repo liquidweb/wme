de9b594bc756cee59847953dffdd0c7aabed7a4f
Mark Furrow <mark@tri.be>
2022-04-11 11:03:00 -0700

    Chore: Improved SetupCardHeader stories

--- src/components/setup-card-header/setup-card-header.stories.tsx
+++ src/components/setup-card-header/setup-card-header.stories.tsx
@@ -10,6 +10,26 @@ export default {
   title: 'Components/SetupCardHeader',
   component: SetupCardHeader,
   decorators: [(story) => <div style={{ maxWidth: 800, margin: '0 auto' }}>{story()}</div>],
+  parameters: {
+    docs: {
+      description: {
+        component: 'The Wizard header features the logo and the ability to exit.',
+      },
+    },
+  },
+  argTypes: {
+    title: {
+      type: 'string',
+      description: '`node` The content of the component.',
+    },
+    subheader: {
+      description: '`node` The content of the component.',
+    },
+    action: {
+      control: false,
+      description: '`node` The action to display in the card header.',
+    },
+  },
 } as ComponentMeta<typeof SetupCardHeader>;
 
 const Template: ComponentStory<typeof SetupCardHeader> = (args) => (
