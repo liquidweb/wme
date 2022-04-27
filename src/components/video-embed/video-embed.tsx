a31ada3450da1cc742814dfd7007326e34e626f2
coltenvantussenbrook <coltenvantussenbrook@gmail.com>
2022-04-27 15:33:44 -0600

    add video to setup card, fix height and width

--- src/components/video-embed/video-embed.tsx
+++ src/components/video-embed/video-embed.tsx
@@ -1,44 +1,50 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import { CardMedia, CardMediaProps } from '@mui/material';
+import { CardMedia, CardMediaProps, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 interface VideoEmbedProps extends CardMediaProps {
   frameBorder?: number | string;
   allow?: string;
-  height?: number | string;
-  width?: number | string;
+  height?: string;
+  width?: string;
   component?: string;
 }
+
+const StyledContainer = styled(Box, {
+  name: 'WmeVideoEmbed',
+  slot: 'Root',
+})(() => ({
+  overflow: 'hidden',
+  paddingBottom: '56.25%',
+  position: 'relative',
+  height: 0,
+}));
 
 const StyledMedia = styled(CardMedia, {
   name: 'WmeVideoEmbed',
   slot: 'Root',
+  shouldForwardProp: (prop) => prop !== 'width' && prop !== 'height',
 })<VideoEmbedProps>(({ width, height }) => ({
-  overflow: 'hidden',
-  paddingBottom: '56.25%',
-  position: 'relative',
-  width,
-  height,
-
-  '& iframe': {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-  },
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  height: height || '100%',
+  width: width || '100%',
 }));
 
 const VideoEmbed: React.FC<VideoEmbedProps> = (props) => {
   const videoProps = {
     frameBorder: 0,
     allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
-    height: 315,
-    width: 560,
     component: 'iframe',
     ...props,
   };
 
   return (
-    <StyledMedia {...videoProps} />
+    <StyledContainer>
+      <StyledMedia {...videoProps} />
+    </StyledContainer>
   );
 };
 
