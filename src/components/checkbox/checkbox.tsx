2d675de36c93e5aebfdb8101f941a3c5bf28b680
Alex Delgado <alex@mangomedia.group>
2022-06-03 11:06:45 -0500

    Refactor 'Checkbox' component

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -1,65 +1,9 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import { Checkbox as MuiCheckbox, FormGroup } from '@mui/material';
-import { FormControlLabel, InputLabel, ErrorText } from '..';
+import React from "react";
+import { FormControlLabelProps } from "@mui/material";
+import { FormControlLabel, CheckboxInput } from "..";
 
-interface CheckboxProps {
-  inputLabel?: string;
-  label: string;
-  error?: boolean;
-  errorMessage?: string;
-}
-
-const StyledCheckbox = styled(MuiCheckbox, {
-  name: 'WmeCheckbox',
-  slot: 'Root',
-})(({ theme }) => ({
-  '&.Mui-checked': {
-    '& .MuiSvgIcon-root': {
-      fill: theme.palette.primary.main,
-    },
-  },
-  '&:focus, &:hover': {
-    '& .MuiSvgIcon-root': {
-      fill: theme.palette.primary.dark,
-    },
-  },
-  '& .MuiSvgIcon-root': {
-    fill: theme.palette.text.disabled,
-  },
-  '&:hover': {
-    backgroundColor: 'transparent',
-  },
-}));
-
-const Checkbox: React.FC<CheckboxProps> = (props) => {
-  const {
-    inputLabel,
-    error,
-    errorMessage,
-    ...rest
-  } = props;
-
-  return (
-    <FormGroup>
-      {
-        inputLabel
-        && <InputLabel>{inputLabel}</InputLabel>
-      }
-      <FormControlLabel
-        control={(
-          <StyledCheckbox
-            className={StyledCheckbox.displayName}
-          />
-        )}
-        {...rest}
-      />
-      {
-        (error && errorMessage)
-        && <ErrorText sx={{ marginTop: 0 }}>{errorMessage}</ErrorText>
-      }
-    </FormGroup>
-  );
+const Checkbox: React.FC<FormControlLabelProps> = (props) => {
+  return <FormControlLabel {...props} control={<CheckboxInput />} />;
 };
 
 export default Checkbox;
