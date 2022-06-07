d71252e76b6f7cac519876df8bd5452c96dece30
Alex Delgado <alex@mangomedia.group>
2022-06-06 20:10:31 -0500

    Add 'Form' component

---
+++ src/components/form/form.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, BoxProps, styled } from "@mui/material";
+
+const StyledForm = styled(Box, {
+  name: "WmeForm",
+  slot: "Root",
+})({}) as typeof Box;
+
+const Form: React.FC<BoxProps> = ({ children, ...props }) => (
+  <StyledForm
+    autoComplete="off"
+    className={(StyledForm as any).displayName}
+    component="form"
+    noValidate
+    {...props}
+  >
+    {children}
+  </StyledForm>
+);
+
+export default Form;
