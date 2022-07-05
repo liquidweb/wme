9369156ef3344980513b241f907e59c6c7bfd1a2
Mark Furrow <mark@tri.be>
2022-07-05 08:33:51 -0700

    Increase spacing between SelectCard icon and title elements

--- src/components/card-select-item/card-select-item.tsx
+++ src/components/card-select-item/card-select-item.tsx
@@ -100,7 +100,7 @@ const StyleCardSelectIcon = styled('div', {
 })(({ theme }) => ({
   width: theme.spacing(7),
   height: theme.spacing(7),
-  marginBottom: theme.spacing(1),
+  marginBottom: theme.spacing(2),
   borderRadius: '50%',
   backgroundColor: theme.palette.grey[100],
   display: 'flex',
