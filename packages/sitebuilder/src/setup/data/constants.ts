2288aca5d7729c6d3a716d5fbba0664306d15dfd
Mark Furrow <mark@tri.be>
2022-10-20 20:44:01 -0700

    feature: SiteBuilder setup screen content change

--- packages/sitebuilder/src/setup/data/constants.ts
+++ packages/sitebuilder/src/setup/data/constants.ts
@@ -3,7 +3,7 @@ import { __ } from '@wordpress/i18n';
 export const SetupData = {
 	screen: {
 		title: __('Set up your site.', 'nexcess-mapps'),
-		intro: __('Our set up wizard will help you get the most out of your store.', 'nexcess-mapps')
+		intro: __('Make your site look good and give it a domain.', 'nexcess-mapps')
 	},
 	goLiveStatus: {
 		manage: __('Manage', 'nexcess-mapps'),
