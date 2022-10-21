da7d019ed0b53c8ce18fadf9f14b619b6c70adb4
Mark Furrow <mark@tri.be>
2022-10-21 07:37:02 -0700

    fix: Correcting test expectation

--- packages/wme-utils/src/pxToRem/index.test.js
+++ packages/wme-utils/src/pxToRem/index.test.js
@@ -2,8 +2,8 @@ const { pxToRem } = require('../../dist/es5/pxToRem');
 
 describe('pxToRem', () => {
   test('true', () => {
-    expect(pxToRem(12)).toBe('0.75 rem');
-    expect(pxToRem(12, 14)).toBe('0.8571 rem');
+    expect(pxToRem(12)).toBe('0.75rem');
+    expect(pxToRem(12, 14)).toBe('0.8571rem');
   });
 
   test('false', () => {
