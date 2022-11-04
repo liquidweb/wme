650c6ba6b9720e7dc35aa06dee013bb514868bdd
jorgemasta <jorgems93@gmail.com>
2022-11-04 09:52:36 +0000

    refactor(domains): rename nexcess to purchase

--- packages/sitebuilder/src/hooks/useGoLive.ts
+++ packages/sitebuilder/src/hooks/useGoLive.ts
@@ -9,7 +9,7 @@ export function useGoLive() {
 		submitDomainVerification,
 		setIsLoading,
 		setHasDomain,
-		setShowNexcessNavigation,
+		setShowPurchaseNavigation,
 		handleDomainVerificationRequest,
 		getHasDomainNextText
 	} = useContext(GoLiveContext) as GoLiveProviderContextInterface;
@@ -20,7 +20,7 @@ export function useGoLive() {
 		submitDomainVerification,
 		setIsLoading,
 		setHasDomain,
-		setShowNexcessNavigation,
+		setShowPurchaseNavigation,
 		handleDomainVerificationRequest,
 		getHasDomainNextText
 	};
