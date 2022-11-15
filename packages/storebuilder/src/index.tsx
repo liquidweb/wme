76b45b8f95d11bffcdea1030d766a388926650d7
jorgemasta <jorgems93@gmail.com>
2022-11-15 11:26:58 +0000

    chore: downgrade to React17 and use rollup in storebuilder

--- packages/storebuilder/src/index.tsx
+++ packages/storebuilder/src/index.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
 import StoreBuilder from './StoreBuilder';
+export default StoreBuilder;
 
-const container = document.getElementById('sitebuilder-store-details-react');
-const root = ReactDOM.createRoot(container!);
-
-root.render(
-	<React.StrictMode>
-		<StoreBuilder />
-	</React.StrictMode>
-);
