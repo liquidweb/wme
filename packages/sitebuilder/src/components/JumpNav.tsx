cf27eae75db881606b60ce1343f75fe62f7937da
Luke Frauhiger <lukefrogger@gmail.com>
2022-12-14 14:04:16 -0600

    Better JumpNav animations and scrolling. Chip style update so it matches Figma

--- packages/sitebuilder/src/components/JumpNav.tsx
+++ packages/sitebuilder/src/components/JumpNav.tsx
@@ -1,34 +1,45 @@
 import { JumpNav as WmeJumpNav } from '@moderntribe/wme-ui';
 import { SetupCardAccordionInterface } from '@sb/setup';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-interface JumpNavInterface {
+export interface JumpNavInterface {
 	cards: SetupCardAccordionInterface[];
 	onItemClick: (index: number) => void;
+}
+
+interface JumpNavItemInterface {
+	id: string;
+	label: string;
+	onClick: () => void;
+	remainingTasks: number;
 }
 
 const JumpNav = (props: JumpNavInterface) => {
 	const { cards, onItemClick } = props;
 
-	const [linksWithNav] = useState(
-		cards.map((card, index) => {
-			const remainingTasks = card.rows?.filter((row) => row.type === 'task' && ! row.completed);
+	const [links, setLinks] = useState<JumpNavItemInterface[]>([]);
 
-			return {
-				id: card.id,
-				label: card.navTitle,
-				onClick: () => onItemClick(index),
-				remainingTasks: remainingTasks.length
-			};
-		})
-	);
+	useEffect(() => {
+		if (cards && cards.length > 0) {
+			setLinks(cards.map((card, index) => {
+				const remainingTasks = card.rows?.filter((row) => row.type === 'task' && ! row.completed);
+
+				return {
+					id: card.id,
+					label: card.navTitle,
+					onClick: () => onItemClick(index),
+					remainingTasks: remainingTasks.length
+				};
+			}));
+		}
+	}, [cards]);
 
 	if (! cards || cards.length === 0) {
 		return <div />;
 	}
 
 	return (
-		<WmeJumpNav title="Jump To:" links={ linksWithNav } />
+		<WmeJumpNav title="Jump To:" links={ links } />
 	);
 };
 
--- packages/sitebuilder/src/setup/SetupCards.tsx
+++ packages/sitebuilder/src/setup/SetupCards.tsx
@@ -1,7 +1,8 @@
 import { Box } from '@mui/material';
 import { SetupCardTasks, SetupCardFooter } from '@sb/setup';
 import { CARDS } from '@sb/constants';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { flushSync } from 'react-dom';
 import { SetupCardAccordion, SetupCardContent } from '@moderntribe/wme-ui';
 import { JumpNav } from '@sb/components';
 
@@ -9,49 +10,63 @@ export interface SetupCardAccordionInterface extends SetupCardInterface {
 	expanded: boolean;
 }
 
+const ACCORDION_TIMEOUT = 150;
+
 const SetupCards = ({ showJumpNav } : { showJumpNav: boolean }) => {
 	const listRef = useRef<HTMLElement | null>(null);
-	const [cardList, setCardList] = useState<SetupCardAccordionInterface[]>(CARDS.map((card) => ({ ...card, expanded: false })));
+	const [cardList, setCardList] = useState<SetupCardAccordionInterface[]>([]);
+
+	useEffect(() => {
+		if (CARDS) {
+			setCardList(CARDS.map((card) => ({ ...card, expanded: false })));
+		}
+	}, [CARDS]);
 
 	const scrollToAndOpen = (index: number) => {
-		toggleAccordion(cardList[ index ].id);
-		listRef.current?.children?.[ index ].scrollIntoView({ behavior: 'smooth' });
+		toggleAccordion(cardList[ index ].id, listRef.current?.children?.[ index ]);
 	};
 
-	const toggleAccordion = (id?: string) => {
-		setCardList(cardList.map((card) => {
-			if (card.id === id) {
-				return { ...card, expanded: ! card.expanded };
-			}
-			return card;
-		}));
+	const toggleAccordion = (id?: string, scrollTo?: Element) => {
+		flushSync(() => {
+			setCardList(
+				cardList.map((card) => {
+					// If toggle is fired from JumpNav and is already expanded, do not close it
+					if (card.id === id) {
+						return { ...card, expanded: (! scrollTo || ! card.expanded) ? ! card.expanded : true };
+					}
+					return card;
+				})
+			);
+		});
+		if (scrollTo) {
+			setTimeout(() => scrollTo.scrollIntoView({ behavior: 'smooth' }), ACCORDION_TIMEOUT);
+		}
 	};
 
 	return (
-		<Box sx={ { maxWidth: '800px', margin: 'auto' } } ref={ listRef }>
-			{ showJumpNav && (
-				<Box mb={ 4 }>
-					<JumpNav cards={ cardList } onItemClick={ scrollToAndOpen } />
-				</Box>
-			) }
-			{ cardList.map((card) => (
-				<SetupCardAccordion
-					expanded={ card.expanded }
-					toggleOpen={ toggleAccordion }
-					key={ card.id }
-					id={ card.id }
-					header={ card.title }
-					subHeader={ card.intro }
-					isComplete={ card.completed }
-					chipText={ `${ card.rows?.filter((row) => row.type === 'task' && ! row.completed)?.length }` }
-					chipBackground="primary"
-				>
-					<SetupCardContent>
-						<SetupCardTasks rows={ card.rows } />
-					</SetupCardContent>
-					{ card.footer && <SetupCardFooter footer={ card.footer } /> }
-				</SetupCardAccordion>
-			)) }
+		<Box sx={ { maxWidth: '800px', margin: 'auto' } }>
+			{ showJumpNav && <JumpNav cards={ cardList } onItemClick={ scrollToAndOpen } /> }
+			<Box mt={ 4 } ref={ listRef }>
+				{ cardList.map((card) => (
+					<SetupCardAccordion
+						expanded={ card.expanded }
+						TransitionProps={ { timeout: ACCORDION_TIMEOUT } }
+						toggleOpen={ toggleAccordion }
+						key={ card.id }
+						id={ card.id }
+						header={ card.title }
+						subHeader={ card.intro }
+						isComplete={ card.completed }
+						chipText={ `${ card.rows?.filter((row) => row.type === 'task' && ! row.completed)?.length }` }
+						chipBackground="primary"
+					>
+						<SetupCardContent>
+							<SetupCardTasks rows={ card.rows } />
+						</SetupCardContent>
+						{ card.footer && <SetupCardFooter footer={ card.footer } /> }
+					</SetupCardAccordion>
+				)) }
+			</Box>
 		</Box>
 	);
 };
