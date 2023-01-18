dec61f5ff938f850716c24cc90910943ebb0f0de
aykan <aykanburcak@gmail.com>
2023-01-18 18:14:03 +0300

    fix: Error when copying assets directory

--- plugins/wme-sitebuilder/webpack.mix.js
+++ plugins/wme-sitebuilder/webpack.mix.js
@@ -4,7 +4,7 @@ const webpack = require('webpack'); // eslint-disable-line import/no-extraneous-
 require('laravel-mix-eslint');
 
 const banner = 'This file is part of WME Sitebuilder and was generated automatically';
-const public_path = process.env.MIX_BUILD_DIR || 'wme-sitebuilder/assets';
+const publicPath = process.env.MIX_BUILD_DIR || 'wme-sitebuilder/assets';
 
 /*
  * Prevent Mix from creating a mix-manifest.json file, as we don't need it.
@@ -14,24 +14,21 @@ const public_path = process.env.MIX_BUILD_DIR || 'wme-sitebuilder/assets';
 Mix.manifest.refresh = () => void 0;
 
 // Customize Mix options.
-mix.setPublicPath(public_path)
-	.options({
-		terser: {
-			extractComments: false,
-			terserOptions: {
-				format: {
-					comments: false,
-					preamble: `/** ${ banner } */`,
-				},
+mix.setPublicPath(publicPath).options({
+	terser: {
+		extractComments: false,
+		terserOptions: {
+			format: {
+				comments: false,
+				preamble: `/** ${banner} */`,
 			},
 		},
-	});
+	},
+});
 
 // Customize the Webpack configuration.
 mix.webpackConfig({
-	plugins: [
-		new webpack.BannerPlugin(banner),
-	],
+	plugins: [new webpack.BannerPlugin(banner)],
 	externals: {
 		'@wordpress/element': 'wp.element',
 		'@wordpress/components': 'wp.components',
@@ -44,17 +41,9 @@ mix.webpackConfig({
 	resolve: {
 		alias: {
 			'@sb': __dirname + '/assets/js/store-details',
-		}
+		},
 	},
 });
-
-// Enable BrowserSync.
-if (process.env.MIX_PROXY_URL) {
-	mix.browserSync({
-		proxy: process.env.MIX_PROXY_URL,
-		files: [`${ __dirname }/**/*.js`, `${ __dirname }/dist/**/*.*`],
-	});
-}
 
 // Bundle JavaScript.
 mix.js('assets/js/sitebuilder.js', '/')
@@ -63,5 +52,23 @@ mix.js('assets/js/sitebuilder.js', '/')
 	.eslint()
 	.react();
 
-mix.copyDirectory('../../packages/sitebuilder/dist/assets/', `${public_path}/sitebuilder/`);
-mix.copyDirectory('../../packages/storebuilder/dist/assets/', `${public_path}/store-details/`);
+// Enable BrowserSync.
+if (process.env.MIX_PROXY_URL) {
+	mix.browserSync({
+		proxy: process.env.MIX_PROXY_URL,
+		files: [`${__dirname}/**/*.js`, `${__dirname}/dist/**/*.*`],
+	});
+}
+
+const copyAssets = (source, target) => {
+	const sourceFolder = mix.inProduction() ? 'dist/assets/' : 'src/assets/images/';
+	const targetFolder = mix.inProduction() ? '' : '/images/';
+	mix.copyDirectory(
+		`../../packages/${source}/${sourceFolder}`,
+		`${publicPath}/${target}/${targetFolder}`,
+	);
+};
+
+// Copy assets from packages.
+copyAssets('sitebuilder', 'sitebuilder');
+copyAssets('storebuilder', 'store-details');
