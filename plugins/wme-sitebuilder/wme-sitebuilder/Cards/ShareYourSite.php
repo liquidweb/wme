350a734a2a232dfa8e135d5a2764363d50f52d06
dotjon <jda@burnmedia.co.uk>
2023-04-12 13:47:55 +0100

    Add share site tasks

--- plugins/wme-sitebuilder/wme-sitebuilder/Cards/ShareYourSite.php
+++ plugins/wme-sitebuilder/wme-sitebuilder/Cards/ShareYourSite.php
@@ -4,15 +4,35 @@ namespace Tribe\WME\Sitebuilder\Cards;
 
 class ShareYourSite extends Card {
 
-	public function props() {
-		$details = [
-			'id'       => 'share-your-site',
-			'navTitle' => __( 'Share Your Site', 'wme-sitebuilder' ),
-			'title'    => __( 'Share Your Site', 'wme-sitebuilder' ),
-			'intro'    => __( 'Get started on your goals by completing a few tasks.', 'wme-sitebuilder' ),
-		];
+	/**
+	 * @var string
+	 */
+	protected $admin_page_slug = 'sitebuilder';
 
-		return $details;
+	public function props() {
+		return [
+			'id'        => 'share-your-site',
+			'navTitle'  => __( 'Share Your Site', 'wme-sitebuilder' ),
+			'title'     => __( 'Share Your Site', 'wme-sitebuilder' ),
+			'intro'     => __( 'Get started on your goals by completing a few tasks.', 'wme-sitebuilder' ),
+			'completed' => false,
+			'rows'      => [
+				[
+					'id'    => 'site-visibility',
+					'type'  => 'task',
+					'title' => __( 'Set Site Visibility', 'wme-sitebuilder' ),
+					'intro' => __( 'Set your site to public or private.', 'wme-sitebuilder' ),
+					'url'   => admin_url( 'admin.php?page=site-settings&card=site-visibility' ), // @todo link to open card
+				],
+				[
+					'id'    => 'site-domain',
+					'type'  => 'task',
+					'title' => __( 'Update Your Domain', 'wme-sitebuilder' ),
+					'intro' => __( 'Update your site URL with a custom domain you own.', 'wme-sitebuilder' ),
+					'url'   => admin_url( 'admin.php?page=site-settings&card=site-domain' ), // @todo link to open card
+				]
+			]
+		];
 	}
 
 }
