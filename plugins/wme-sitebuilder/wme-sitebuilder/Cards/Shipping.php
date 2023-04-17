aa689c7337e9de0c74b3feb4f8b3326ff2156932
Brian Watson <bwatson@liquidweb.com>
2023-04-17 00:20:30 -0400

    Remove Shipping Wizard and migrate plugin installation to Shipping Card.

--- plugins/wme-sitebuilder/wme-sitebuilder/Cards/Shipping.php
+++ plugins/wme-sitebuilder/wme-sitebuilder/Cards/Shipping.php
@@ -2,9 +2,12 @@
 
 namespace Tribe\WME\Sitebuilder\Cards;
 
+use Tribe\WME\Sitebuilder\Concerns\InvokesCli;
 use Tribe\WME\Sitebuilder\Plugins\Shipping as ShippingPlugins;
 
 class Shipping extends Card {
+
+	use InvokesCli;
 
 	/**
 	 * @var string
@@ -30,6 +33,67 @@ class Shipping extends Card {
 		$this->plugins = $shipping_plugins;
 
 		parent::__construct();
+
+		add_action( 'admin_init', [$this, 'handle_plugin_install_request'], 2 );
+	}
+
+	/**
+	 * Install requested plugins.
+	 */
+	public function handle_plugin_install_request() {
+		// Ensure this is a request to admin and also not an ajax request.
+		if ( ! is_admin() || wp_doing_ajax() ) {
+			return;
+		}
+
+		// Ensure this is a request to install a plugin.
+		if ( ! isset( $_GET['page'] ) || 'sitebuilder-store-details' !== $_GET['page'] ) {
+			return;
+		}
+
+		if ( ! isset( $_GET['action'] ) || 'install-plugin' !== $_GET['action'] ) {
+			return;
+		}
+
+		// Ensure user has permission to install plugins.
+		if ( ! current_user_can( 'install_plugins' ) ) {
+			wp_die( __( 'You are not authorized to install plugins.', 'wme-sitebuilder' ) );
+		}
+
+		$requested_plugin = sanitize_text_field( $_GET['plugin'] );
+
+		if ( null === $requested_plugin ) {
+			wp_die( __( 'You are not authorized to install plugins.', 'wme-sitebuilder' ) );
+		}
+
+		$supported_plugins = $this->plugins->getPlugins();
+
+		if ( ! isset( $supported_plugins[ $requested_plugin ] ) ) {
+			wp_die( __( 'The requested plugin is not supported by this installer.', 'wme-sitebuilder' ) );
+		}
+
+		if ( ! empty( $supported_plugins[ $requested_plugin ]['active'] ) ) {
+			wp_die( __( 'The requested plugin is already installed and active.', 'wme-sitebuilder' ) );
+		}
+
+		$response = $this->makeCommand('wp plugin install', [
+			$requested_plugin,
+			'--activate',
+		])->execute();
+
+		if ( ! $response->wasSuccessful() ) {
+			wp_die(
+				sprintf(
+				/* Translators: %1$s is the exit code from WP CLI; %2$s is output from WP CLI. */
+					__( 'Encountered WP CLI exit code "%1$s" with output "%2$s".', 'wme-sitebuilder' ),
+					sanitize_text_field( $response->getExitCode() ),
+					sanitize_text_field( $response->getOutput() )
+				)
+			);
+		}
+
+		// Redirect user back to the shipping card.
+		wp_safe_redirect( $supported_plugins[ $requested_plugin ]['activation_redirect'] );
 	}
 
 	/**
@@ -43,7 +107,7 @@ class Shipping extends Card {
 				'label'           => __( 'Add USPS', 'wme-sitebuilder' ),
 				'backgroundColor' => '#004B87',
 				// TODO: Redirect user to an action that installs and activates the plugin.
-				'href'            => admin_url( 'admin.php?page=wc-settings&tab=shipping' ),
+				'href'            => admin_url( 'admin.php?page=sitebuilder-store-details&action=install-plugin&plugin=elex-usps-shipping-method' ),
 			]
 		];
 
